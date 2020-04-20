d9d12c7ef6e7622d19df6c2ffbff99005620c2c4
Robert Merriman <robertbmerriman@gmail.com>
2020-04-20 20:01:56 +0100

    NAISU NAISU BERI NAISU

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Sakura from "./sakura";
 
-function App() {
+//leaf colour = #60c4c3
+
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Sakura leaves={30} />
+      <Sakura leaves={10} zIndex={10} />
+      <div className="card">
+        <div className="col center">
+          <img src="img/robert.merriman.jpg" alt="It's-a me!" className="me" />
+          <h2>Front endgineer</h2>
+          <p>lorem ispem</p>
+        </div>
+        <div className="vr" />
+        <div className="col">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi mollitia deserunt officia quasi vel.
+            Accusantium libero tenetur.
+          </p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor voluptatum</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Autem voluptates quas facere! Magni quis in
+            assumenda eius facilis! Omnis velit consectetur culpa sequi commodi modi?
+          </p>
+          <p>Lorem ipsum, dolor sit amet</p>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/sakura/index.tsx
@@ -0,0 +1,26 @@
+import React, { useEffect } from "react";
+import { LeafScene } from "./scene";
+import "./sakura.css";
+
+interface Props {
+  leaves?: number;
+  zIndex?: number;
+}
+
+const Sakura = ({ leaves = 40, zIndex = -10 }: Props) => {
+  const className = "sakura";
+  const uid = `${performance.now()}`;
+  const id = `${className}-${uid.replace(".", "")}`;
+
+  useEffect(() => {
+    const leafContainer = document.querySelector(`#${id}`),
+      leafScene = new LeafScene(leafContainer, leaves);
+
+    leafScene.init();
+    leafScene.render();
+  });
+
+  return <div className={className} id={id} style={{ zIndex }}></div>;
+};
+
+export default Sakura;
