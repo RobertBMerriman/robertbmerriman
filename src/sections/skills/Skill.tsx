38446bf139aa32f634a376952a97cde4c1bf5e9c
Robert Merriman <robertbmerriman@gmail.com>
2020-11-24 17:34:17 +0000

    Add more skill examples and name to skill

--- src/sections/skills/Skill.tsx
+++ src/sections/skills/Skill.tsx
@@ -6,19 +6,21 @@ import { Col } from "components/Col";
 import { VerticalDivider } from "components/VerticalDivider";
 
 interface Props {
-  icon: IconDefinition
+  icon?: IconDefinition
+  name: string
   rating: 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10
   description: React.ReactNode
 }
 
-const ratingBlock: React.CSSProperties = { width: 10, height: 'auto', margin: '5px 1px', boxSizing: 'border-box', borderRadius: 3 }
+const ratingBlock: React.CSSProperties = { width: 10, height: 'auto', margin: '8px 1px', boxSizing: 'border-box', borderRadius: 3 }
 
-export const Skill = ({ icon, rating, description }: Props) =>
+export const Skill = ({ icon, name, rating, description }: Props) =>
   <Row>
     <Col right>
       <Col center>
         <Row>
-          <FontAwesomeIcon icon={icon} size="4x" />
+          {icon && <FontAwesomeIcon icon={icon} size="3x" />}
+          <Col center>{name.toUpperCase()}</Col>
           {Array(rating).fill(0).map(() => <div style={{ ...ratingBlock, background: '#87c0d2' }} />)}
           {Array(10 - rating).fill(0).map(() => <div style={{ ...ratingBlock, border: '2px #87c0d2 solid' }} />)}
         </Row>
--- src/sections/skills/index.tsx
+++ src/sections/skills/index.tsx
@@ -1,11 +1,35 @@
 import React from 'react'
 import { Section } from "components/Section";
-import { faJava, faJsSquare } from "@fortawesome/free-brands-svg-icons";
+import { faAndroid, faApple, faJava, faJsSquare, faPython, faReact, faSwift } from "@fortawesome/free-brands-svg-icons";
 import { Skill } from "./Skill";
+import { faGem } from '@fortawesome/free-regular-svg-icons';
 
 
 export const SkillsSection = () =>
   <Section>
-    <Skill icon={faJsSquare} rating={8} description={<p>Ooooohhhhhhhh I'm bloody good mate</p>} />
-    <Skill icon={faJava} rating={4} description={<p>Ooooohhhhhhhh I'm bloody okay mate</p>} />
+    <Skill icon={faJsSquare} name="Javascript" rating={8} description={<p>Ooooohhhhhhhh I'm bloody good mate</p>} />
+    <Skill name="Typescript" rating={8} description={<p>Ooooohhhhhhhh I'm bloody good mate</p>} />
+    <Skill icon={faReact} name="React" rating={8} description={<p>Ooooohhhhhhhh I'm bloody good mate</p>} />
+    <br />
+    <Skill icon={faPython} name="Python" rating={5} description={<p>Ooooohhhhhhhh I'm bloody okay mate</p>} />
+    <Skill name="Django" rating={5} description={<p>Ooooohhhhhhhh I'm bloody okay mate</p>} />
+    <Skill name="Flask" rating={5} description={<p>Ooooohhhhhhhh I'm bloody okay mate</p>} />
+    <br />
+    <Skill icon={faGem} name="Ruby" rating={5} description={<p>Ooooohhhhhhhh I'm bloody okay mate</p>} />
+    <Skill name="Rails" rating={5} description={<p>Ooooohhhhhhhh I'm bloody okay mate</p>} />
+    <Skill name="Sinatra" rating={5} description={<p>Ooooohhhhhhhh I'm bloody okay mate</p>} />
+    <br />
+    <Skill icon={faAndroid} name="Android" rating={5} description={<p>Ooooohhhhhhhh I'm bloody okay mate</p>} />
+    <Skill name="Kotlin" rating={5} description={<p>Ooooohhhhhhhh I'm bloody okay mate</p>} />
+    <Skill icon={faJava} name="Java" rating={4} description={<p>Ooooohhhhhhhh I'm bloody okay mate</p>} />
+    <br />
+    <Skill icon={faApple} name="iOS" rating={5} description={<p>Ooooohhhhhhhh I'm bloody okay mate</p>} />
+    <Skill icon={faSwift} name="Swift" rating={5} description={<p>Ooooohhhhhhhh I'm bloody okay mate</p>} />
+    <br />
+
+
+    <h3>Soft skills</h3>
+    <p>Teaching - I have really enjoyed tutoring more junior devs and have got lots of positive feedback. ... This is something I'd like to do even more of.</p>
+    <p>Non-technical communication - I make an effort to understand my audience when presenting ideas or handing off important information and try to cut down unnessicary technical details without sacrificing the overall point when talking to non-technical team-members or product stakeholders.<br />
+    Once I had someone say "Thank god it was you explaining that, I have no idea what the other engienners are saying sometimes" </p>
   </Section>
